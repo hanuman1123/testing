5537a7f0d5fee6a3bc79eab03284fdee4fc887a0
hanuman1123 <jagadeeshhanumantadi@gmail.com>
2025-10-24 14:07:03 -0400

    Fix: Rename login.jsx to Login.jsx

--- mychatapp-frontend/src/App.jsx
+++ mychatapp-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
-import Login from "./pages/login";
+import Login from "./pages/login_temp";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import PrivateRoute from "./routes/PrivateRoute";
